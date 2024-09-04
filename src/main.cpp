b96b6f43588a4619d31b48655e7102702bf47f35
imanoob1122 <ryosuke1122@keio.jp>
2024-09-04 21:31:23 +0900

    mainに受信用プログラム追加

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,37 @@
 #include "Motor.hpp"
 
 CAN can1(PB_8, PB_9, 1000000);
+static UnbufferedSerial esp32(PA_0, PA_1, 115200);
+DigitalOut led(LED1);
+char data[8] = {0};
+Motor Lmotor(can1, 1, data, true);
+char buf[2];
+char str[64];
+unsigned int test;
+
+void read_esp() {
+    static int i = 0;
+    esp32.read(buf, 2);
+    if (i >= 64) {
+        i = 0;
+        memset(str, '\0', sizeof(str));
+    }
+    if (buf[0] == '\n') {
+        i=0;
+        sscanf(str, "0x%04x\n", &test);
+        memset(str, '\0', sizeof(str));
+    }
+    else {
+        led = !led;
+        str[i] = buf[0];
+        i++;
+    }
+}
 
 
 int main() {
     printf("start");
-    char data[8] = {0};
-    Motor Lmotor(can1, 1, data, true);
+    esp32.attach(&read_esp, UnbufferedSerial::RxIrq);
     Lmotor.set_current(100);
-    printf("test");
-}
+
+    }
