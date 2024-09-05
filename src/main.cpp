950b1adbe9029e471d8ece13cae86c886b2f63ad
imanoob1122 <ryosuke1122@keio.jp>
2024-09-06 08:58:10 +0900

    ii

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
-#include "mbed.h"
-#include <CAN.h>
-#include "Motor.hpp"
+#include <mbed.h>
 
-CAN can1(PB_8, PB_9, 1000000);
-static UnbufferedSerial esp32(PA_0, PA_1, 115200);
+static UnbufferedSerial esp32(PA_9, PA_10, 115200);
 DigitalOut led(LED1);
-char data[8] = {0};
-Motor Lmotor(can1, 1, data, true);
+PwmOut l_motor(PA_0);
+PwmOut r_motor(PA_1);
+//PwmOut arm();
+DigitalOut chack1(PA_6);
+DigitalOut chack2(PA_7);
+DigitalOut chack3(PA_8);
+int speed;
 char buf[2];
 char str[64];
 unsigned int test;
+
 
 void read_esp() {
     static int i = 0;
@@ -30,10 +33,72 @@ void read_esp() {
     }
 }
 
+void setup() {
+     for(float i=0.16f; i<=0.38; i+=0.01f) {
+         r_motor.write(i-0.03);
+         l_motor.write(i);
+         ThisThread::sleep_for(1000ms);
+     }
+     for(float i=0.38f; i>=0.16; i-=0.01f) {
+         r_motor.write(i);
+         l_motor.write(i-0.03);
+         ThisThread::sleep_for(1000ms);
+     }
+    r_motor.write(0.22);
+}
 
 int main() {
-    printf("start");
+    speed = 1500;
     esp32.attach(&read_esp, UnbufferedSerial::RxIrq);
-    Lmotor.set_current(100);
+    l_motor.period(0.002f);
+    r_motor.period(0.002f);
+    //arm.period_ms(5);
 
+    setup();
+    
+    while(true) {
+        printf("%x\n", test);
+        switch(test){
+            case 0:
+                l_motor.write(0.23f);
+                r_motor.write(0.27f);
+                //arm.pulsewidth(0);
+                break;
+            case 8:
+                l_motor.write(0.19f);
+                r_motor.write(0.32f);
+                break;
+            case 1:
+                l_motor.write(0.29f);
+                r_motor.write(0.22f);
+                break;
+            case 2:
+                l_motor.write(0.19f);
+                r_motor.write(0.22f);
+                break;
+            case 4:
+                l_motor.write(0.29f);
+                r_motor.write(0.32f); 
+                break;               
+            case 10: 
+                   
+                //chack1.write(0);
+                //chack2.write(0);
+                //chack3.write(0);
+                
+                break;
+            case 20:
+                //chack1.write(1);
+                //chack2.write(1);
+                //chack3.write(1);
+                break;
+            case 80:
+                //arm.pulsewidth(1600);
+            case 40:
+                //arm.pilsewidth(1400);
+                break;
+        }
+        
     }
+    
+}
