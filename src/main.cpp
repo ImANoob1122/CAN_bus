b2dc8583752aea0930916a03ce4c8034de680414
imanoob1122 <ryosuke1122@keio.jp>
2024-09-06 10:38:44 +0900

    iikanji

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,21 @@ static UnbufferedSerial esp32(PA_9, PA_10, 115200);
 DigitalOut led(LED1);
 PwmOut l_motor(PA_0);
 PwmOut r_motor(PA_1);
-//PwmOut arm();
+PwmOut Movingarm(PA_2);
 DigitalOut chack1(PA_6);
 DigitalOut chack2(PA_7);
 DigitalOut chack3(PA_8);
-int speed;
 char buf[2];
 char str[64];
 unsigned int test;
+
+//モーターなどの変数
+float Lmotor_middle = 0.23f;    //モーターのPWMはこれを中心に+-11のみ受け付ける
+float Rmotor_middle = 0.27f;
+float arm_middle = 0.0f;
+float speed = 0.05f; //モーターを動かす速度
+
+
 
 
 void read_esp() {
@@ -33,6 +40,7 @@ void read_esp() {
     }
 }
 
+//キャリブレーションってやつ
 void setup() {
      for(float i=0.16f; i<=0.38; i+=0.01f) {
          r_motor.write(i-0.03);
@@ -44,48 +52,121 @@ void setup() {
          l_motor.write(i-0.03);
          ThisThread::sleep_for(1000ms);
      }
-    r_motor.write(0.22);
+}
+
+//モーターの動作を確認する
+void check_middle(PwmOut& checking_motor, int pre_middle) {
+    while(true) {
+        int k = pre_middle - 11;
+        for(float i = (pre_middle - 11)/100.0f; i<=pre_middle + 11; i += 0.01f) {
+            checking_motor.write(i);
+            printf("%d\n", k);
+            k++;
+            ThisThread::sleep_for(1000ms);
+        }
+        k = pre_middle + 11;
+        for(float i = (pre_middle + 11)/100.0f; i>=pre_middle - 11; i -= 0.01f) {
+            checking_motor.write(i);
+            printf("%d\n", k);
+            k--;
+            ThisThread::sleep_for(1000ms);
+        }
+    }
+}
+
+//ロボットの停止
+void stop() {
+    l_motor.write(Lmotor_middle);
+    r_motor.write(Rmotor_middle);
+}
+
+//ロボットの直進
+void forward() {
+    l_motor.write(Lmotor_middle-speed);
+    r_motor.write(Rmotor_middle+speed);
+}
+
+//ロボットの後退
+void back() {
+    l_motor.write(Lmotor_middle+speed);
+    r_motor.write(Rmotor_middle-speed);
+}
+
+//ロボットの右回転
+void Rturn() {
+    l_motor.write(Lmotor_middle-speed);
+    r_motor.write(Rmotor_middle-speed);
+}
+
+//ロボットの左回転
+void Lturn() {
+    l_motor.write(Lmotor_middle+speed);
+    r_motor.write(Rmotor_middle+speed);
 }
 
 int main() {
-    speed = 1500;
     esp32.attach(&read_esp, UnbufferedSerial::RxIrq);
     l_motor.period(0.002f);
     r_motor.period(0.002f);
-    //arm.period_ms(5);
+    Movingarm.period(0.002f);
+
+    check_middle(Movingarm, 27); //アームを動かす前にこれを実行して！　中心が分かったらarm_middleを更新してこの行を消すこと
 
     setup();
+
+    int phase = 0;
     
     while(true) {
         printf("%x\n", test);
         switch(test){
             case 0:
-                l_motor.write(0.23f);
-                r_motor.write(0.27f);
-                //arm.pulsewidth(0);
+                stop();
+                Movingarm.write(arm_middle);
                 break;
             case 8:
-                l_motor.write(0.19f);
-                r_motor.write(0.32f);
+                forward();
                 break;
             case 1:
-                l_motor.write(0.29f);
-                r_motor.write(0.22f);
+                back();
                 break;
             case 2:
-                l_motor.write(0.19f);
-                r_motor.write(0.22f);
+                Rturn();
                 break;
             case 4:
-                l_motor.write(0.29f);
-                r_motor.write(0.32f); 
+                Lturn();
                 break;               
-            case 10: 
-                   
-                //chack1.write(0);
-                //chack2.write(0);
-                //chack3.write(0);
-                
+            case 10: //自動化部分　自分好みに調整しよう！
+                if (phase == 1) {
+                    forward();
+                    ThisThread::sleep_for(1000ms);//時間を変更することで上の動作の時間を変えられるよ
+                    phase++;
+                } else if(phase == 2) {
+                    Rturn();
+                    ThisThread::sleep_for(1000ms);
+                    phase++;
+                } else if(phase == 3) {
+                    forward();
+                    ThisThread::sleep_for(1000ms);
+                    phase++;
+                } else if(phase == 4) {
+                    chack1.write(1);
+                    chack2.write(1);
+                    chack3.write(1);
+                    ThisThread::sleep_for(1000ms);
+                    phase++;
+                } else if (phase == 5) {
+                    back();
+                    ThisThread::sleep_for(1000ms);
+                    phase++;
+                } else if (phase == 6) {
+                    Lturn();
+                    ThisThread::sleep_for(1000ms);
+                    phase++;
+                } else if (phase == 7) {
+                    forward();
+                    ThisThread::sleep_for(1000ms);
+                    phase++;
+                }
                 break;
             case 20:
                 //chack1.write(1);
@@ -93,9 +174,10 @@ int main() {
                 //chack3.write(1);
                 break;
             case 80:
-                //arm.pulsewidth(1600);
+                Movingarm.write(arm_middle + speed);
+                break;
             case 40:
-                //arm.pilsewidth(1400);
+                Movingarm.write(arm_middle - speed);
                 break;
         }
         
